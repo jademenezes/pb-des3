f772085b03d5cee107f67ca05624a55271860566
Jade Menezes <jaderegis97@gmail.com>
2025-01-06 17:57:09 -0300

    MongoDB setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StoresModule } from './stores/stores.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [StoresModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+      expandVariables: true,
+    }),
+    MongooseModule.forRoot(process.env.DB_URL),
+    StoresModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
