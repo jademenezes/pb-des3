db842758467ca75f3ecd84145a7cd6d842746b10
Jade Menezes <jaderegis97@gmail.com>
2025-01-07 17:37:52 -0300

    Injected Store model on Store Service

--- src/stores/stores.service.ts
+++ src/stores/stores.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Store } from './stores.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class StoresService {}
+export class StoresService {
+  // Injetar modelo Store
+  constructor(@InjectModel(Store.name) private storeModel: Model<Store>) {}
+}
