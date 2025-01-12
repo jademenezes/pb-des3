fdaa6b0c6d525691fe97abf51040758b2d5de2bc
Jade Menezes <jaderegis97@gmail.com>
2025-01-12 17:04:13 -0300

    Update dtos for Swagger

--- src/stores/dtos/create-store.dto.ts
+++ src/stores/dtos/create-store.dto.ts
@@ -14,43 +14,52 @@ import {
   MinLength,
 } from 'class-validator';
 import { storeType } from '../enums/store-type.enums';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class UserStoreDto {
+  @ApiProperty()
   @IsString()
   @MinLength(5)
   @MaxLength(50)
   storeName: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsBoolean()
   takeOutInStore?: boolean;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsNumber()
   @Min(1)
   @Max(30)
   shippingTimeInDays?: number;
 
+  @ApiProperty()
   @IsString()
   @MinLength(7)
   @MaxLength(50)
   address: string;
 
+  @ApiProperty()
   @IsString()
   type: storeType;
 
+  @ApiProperty()
   @IsString()
   @Length(8)
   postalCode: string;
 
+  @ApiPropertyOptional()
   @IsString()
   @Length(11)
   @IsOptional()
-  telephoneNumber: string;
+  telephoneNumber?: string;
 
+  @ApiPropertyOptional()
   @IsEmail()
   @IsOptional()
-  emailAddress: string;
+  emailAddress?: string;
 }
 
 export class CreateStoreDto {
@@ -103,11 +112,11 @@ export class CreateStoreDto {
   @IsString()
   @Length(11)
   @IsOptional()
-  telephoneNumber: string;
+  telephoneNumber?: string;
 
   @IsEmail()
   @IsOptional()
-  emailAddress: string;
+  emailAddress?: string;
 }
 
 export class GeoResponseDto {
--- src/stores/dtos/update-store.dto.ts
+++ src/stores/dtos/update-store.dto.ts
@@ -11,62 +11,76 @@ import {
   MinLength,
 } from 'class-validator';
 import { storeType } from '../enums/store-type.enums';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class UpdateStoreDto {
+  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   @MinLength(5)
   @MaxLength(50)
   storeName?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsBoolean()
   takeOutInStore?: boolean;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsNumber()
   shippingTimeInDays?: number;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsLatitude()
   latitude?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsLongitude()
   longitude?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   @MinLength(7)
   @MaxLength(50)
   address?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   city?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   state?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   type?: storeType;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   country?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   @Length(8)
   postalCode?: string;
 
+  @ApiPropertyOptional()
   @IsString()
   @Length(11)
   @IsOptional()
   telephoneNumber?: string;
 
+  @ApiPropertyOptional()
   @IsEmail()
   @IsOptional()
   emailAddress?: string;
