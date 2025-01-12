e6828e9e0c01beab5aae4ae16e2544a0fcb4fb32
Jade Menezes <jaderegis97@gmail.com>
2025-01-12 17:03:25 -0300

    Configure Swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './exception-filters/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,7 +10,19 @@ async function bootstrap() {
   // Aplica validação global
   app.useGlobalPipes(new ValidationPipe());
 
+  // Aplica filtro global de erros
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  // Configurações para uso do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('PB Desafio 3 API')
+    .setDescription('Documentação para API do desafio 3 PB Node.js')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
